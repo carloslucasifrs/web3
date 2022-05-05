d4a65675bd3472b295d5d70d90d2882eb8650855
Carlos Eduardo Lucas <carlos.lucas@aluno.riogrande.ifrs.edu.br>
2022-05-05 15:21:59 -0300

    validate roles

--- src/main/java/br/edu/ifrs/riogrande/tads/carloslucas/app/model/Role.java
+++ src/main/java/br/edu/ifrs/riogrande/tads/carloslucas/app/model/Role.java
@@ -29,7 +29,7 @@ public class Role {
     @ToString.Include
     Integer id;
 
-    @Column(name ="role", nullable = false)
+    @Column(name ="role", length = 20, nullable = false, unique = true)
     @ToString.Include
     String role;
 
